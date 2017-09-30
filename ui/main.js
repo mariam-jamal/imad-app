59999fbd6d25c80b2f623bad5049dfa6b59c2dec
mariam-jamal <30799589+mariam-jamal@users.noreply.github.com>
2017-09-30 14:34:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+var img = document.getElementById('madi');
+madi.onclick= function (){
+    madi.style.maginleft = '100px';
+};
